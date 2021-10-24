58bc2ac582464daabb9197add5a32e2b0f42e569
robindano <151894372+robindano@users.noreply.github.com>
2021-10-23 19:08:08 -0600

    Re-configure docker setup to just take a host URL and port

--- app_react/src/App.js
+++ app_react/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  fetch(process.env.REACT_APP_NODE_URL)
+  fetch(process.env.REACT_APP_BACKEND_URL)
     .then(response => response.json())
     .then(data => console.log(data))
   return (
